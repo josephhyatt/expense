9c61f26a51827f7498118815ec2a7c97ef2e16ea
Joseph Hyatt <josephnhyatt@gmail.com>
2021-05-28 14:43:39 -0700

    imported main javascript file to app.js

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./Main.css";
+
+export default class Main extends Component {
+    render() {
+        return(<>Main</>)
+    }
+}
