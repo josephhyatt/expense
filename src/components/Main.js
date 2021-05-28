3ab13571322792a8f134c00157f81daaa3cb597d
Joseph Hyatt <josephnhyatt@gmail.com>
2021-05-28 15:19:11 -0700

    added onClick function to not registered button

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,16 @@ export default class Main extends Component {
 	state = {
 		user: 1,
 		loading: true,
+		formSwitcher: false,
+	};
+
+	// switch between login and register forms
+	formSwitcher = (action) => {
+		console.log(action);
+		// if formSwitcher action (parameter) is equal to register make it true
+		this.setState({
+			formSwitcher: action === "register" ? true : false,
+		});
 	};
 
 	render() {
@@ -15,10 +25,38 @@ export default class Main extends Component {
 			<>
 				<div className="mainBlock">
 					<Login />
-					<span className="underline">
-						Not Registered?{" "}
-						<button className="linkBtn">Create a user account</button>
-					</span>
+					{/* If formSwitcher is false show "Not Registered Login Form */}
+					{!this.state.formSwitcher ? (
+						<span className="underline">
+							Not Registered?{" "}
+							<button
+								// if formSwitcher is false send parameter false otherwise login
+								onClick={() =>
+									this.formSwitcher(
+										!this.state.formSwitcher ? "register" : "login"
+									)
+								}
+								className="linkBtn"
+							>
+								Create a user account
+							</button>
+						</span>
+					) : (
+						<span className="underline">
+							Have an Account?{" "}
+							<button
+								// if formSwitcher is false send parameter false otherwise login
+								onClick={() =>
+									this.formSwitcher(
+										!this.state.formSwitcher ? "register" : "login"
+									)
+								}
+								className="linkBtn"
+							>
+								Sign in here
+							</button>
+						</span>
+					)}
 				</div>
 			</>
 		);
