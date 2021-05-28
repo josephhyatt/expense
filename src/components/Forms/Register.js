1cd52ef09889bcad9ed2320399b7ac778394ffb9
Joseph Hyatt <josephnhyatt@gmail.com>
2021-05-28 15:52:50 -0700

    added error handling with firebase

--- src/components/Forms/Register.js
+++ src/components/Forms/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import fire from "../../config/Fire";
 import "./Login.css";
 
 class Register extends Component {
@@ -9,6 +10,30 @@ class Register extends Component {
 		fireErrors: "",
 	};
 
+	// handles user state to handle user login/register change with firebase
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	// handles event when submit gets clicked on login/register form
+	register = (event) => {
+		event.preventDefault();
+		fire
+			.auth()
+			.createUserWithEmailAndPassword(this.state.email, this.state.password)
+			.then((user) => {
+				var currentUser = fire.auth().currentUser;
+				currentUser.updateProfile({
+					displayName: this.state.displayName,
+				});
+			})
+			.catch((error) => {
+				this.setState({ fireErrors: error.message });
+			});
+	};
+
 	render() {
 		return (
 			<>
@@ -17,13 +42,17 @@ class Register extends Component {
 						type="text"
 						className="regField"
 						placeholder="Your Name"
-						name="name"
+						onChange={this.handleChange}
+						value={this.state.displayName}
+						name="displayName"
 					/>
 
 					<input
 						type="text"
 						className="regField"
 						placeholder="Email"
+						onChange={this.handleChange}
+						value={this.state.email}
 						name="email"
 					/>
 
@@ -31,10 +60,13 @@ class Register extends Component {
 						type="password"
 						className="regField"
 						placeholder="Password"
+						onChange={this.handleChange}
+						value={this.state.password}
 						name="password"
 					/>
 
 					<input
+						onClick={this.register}
 						type="submit"
 						className="submitBtn"
 						value="REGISTER"
