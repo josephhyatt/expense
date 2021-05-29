eb72edcbf21788d882285bad503ba89f0d0465a8
Joseph Hyatt <josephnhyatt@gmail.com>
2021-05-29 09:46:36 -0700

    login/registration now working and firebase storing user creds

--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Login.css";
+import fire from "../../config/Fire";
 
 class Login extends Component {
 	state = {
@@ -8,14 +9,37 @@ class Login extends Component {
 		fireErrors: "",
 	};
 
+	// waits for event to happen
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	// creating login state, and catch errors
+	login = (event) => {
+		event.preventDefault();
+		fire
+			.auth()
+			.signInWithEmailAndPassword(this.state.email, this.state.password)
+			.catch((error) => {
+				this.setState({ fireErrors: error.message });
+			});
+	};
+
 	render() {
+		// looks out for errors and displays them to user
+		let errorNotification = this.state.fireErrors ? (
+			<div className="Error"> {this.state.fireErrors} </div>
+		) : null;
 		return (
 			<>
+				{errorNotification}
 				<form>
 					<input
 						type="text"
 						className="regField"
 						placeholder="Email"
+						value={this.state.email}
+						onChange={this.handleChange}
 						name="email"
 					/>
 
@@ -23,10 +47,13 @@ class Login extends Component {
 						type="password"
 						className="regField"
 						placeholder="Password"
+						value={this.state.password}
+						onChange={this.handleChange}
 						name="password"
 					/>
 
 					<input
+						onClick={this.login}
 						type="submit"
 						className="submitBtn"
 						value="ENTER"
