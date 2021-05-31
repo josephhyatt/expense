eb729e2ec470bc05b443fb16bd9a4ff7e63b3d90
Joseph Hyatt <josephnhyatt@gmail.com>
2021-05-31 08:45:18 -0700

    adding latest transaction area

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -18,6 +18,38 @@ class Tracker extends Component {
 						Exit
 					</button>
 				</div>
+				<div className="totalMoney">$145</div>
+
+				<div className="newTransactionBlock">
+					<div className="newTransaction">
+						<form>
+							<input
+								placeholder="Transaction Name"
+								type="text"
+								name="transactionName"
+							/>
+							<div className="inputGroup">
+								<select name="type">
+									<option value="0">Type</option>
+									<option value="expense">Expense</option>
+									<option value="deposit">Deposit</option>
+								</select>
+								<input placeholder="Price" type="text" name="Price" />
+							</div>
+							<button className="addTransaction">+ Add Transaction</button>
+						</form>
+					</div>
+				</div>
+
+                <div className="latestTransactions">
+                    <p>Latest Transactions</p>
+                    <ul>
+                        <li>
+                            <div>ATM Deposit</div>
+                            <div>+$5</div>
+                        </li>
+                    </ul>
+                </div>
 			</div>
 		);
 	}
